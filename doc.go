7381038019d89f079af30744f7608038faba1127
chai2010 <chaishushan@gmail.com>
2016-02-18 13:29:10 +0800

    zz

---
+++ doc.go
@@ -0,0 +1,12 @@
+// Copyright 2015 Golang-China. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//
+// Go圣经中文版.
+//
+// 项目主页: https://bitbucket.org/golang-china/gopl-zh
+//
+// 原版官网: http://gopl.io
+//
+package gopl_zh
